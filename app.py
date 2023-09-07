6022d7291f8229f7b2bdd575f3f98254a9d9d283
Francicley Menezes <f.menezesqa@gmail.com>
2023-09-07 00:11:31 +0000

    mensagem

--- app.py
+++ app.py
@@ -1,18 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session, flash, redirect, url_for
 
-app = Flask ("Meu App")
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'pudim'
+
 posts = [
     {
-        "titulo": "Minha Primeira Postagem",
+        "titulo": "Minha primeira postagem",
         "texto": "teste"
     },
     {
         "titulo": "Segundo Post",
-        "texto": "Outro teste"
+        'texto': "outro teste"
     }
 ]
-@app.route ('/')
+
+@app.route('/')
 def exibir_entradas():
-    entradas = posts #Mock das postagens
-    return render_template ("exibir_entradas.html", entradas=entradas)
+    entradas = posts # Mock das postagens
+    return render_template('exibir_entradas.html', entradas=entradas)
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    erro = None
+    if request.method == "POST":
+        if request.form['username'] == "admin" and request.form['password'] == "admin":
+            session['logado'] = True
+            flash("Login efetuado com sucesso!")
+            return redirect(url_for('exibir_entradas'))
+        erro = "Usuário ou senha inválidos"        
+    return render_template('login.html', erro=erro)
 
